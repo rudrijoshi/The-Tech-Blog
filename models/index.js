e45d55ae58bc5571e531f939ca95cf23fe963620
Mona Lisa <130613163+rudrijoshi@users.noreply.github.com>
2023-06-28 19:45:26 -0400

    added text to index.js file in models

--- models/index.js
+++ models/index.js
@@ -1,6 +1,6 @@
 const User = require('./User');
 const Blog = require('./Blog');
-const Comment = require('./comment');
+const Comment = require('./Comment');
 
 User.hasMany(Blog, {
     foreignKey: 'user_id',
